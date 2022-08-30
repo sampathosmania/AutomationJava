acab38bfcc2c2c8eb7a5fb311d7ed48490a7b2f9
Sampath Kumar Bodapatla <sampathukumar@gmail.com>
2022-08-30 14:57:41 +0530

    First commit...

---
+++ JavaAutomation/src/JavaPrograms/Day2DataTypes.java
@@ -0,0 +1,13 @@
+package JavaPrograms;
+
+public class Day2DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=10;
+float y=29.34f;
+System.out.println(x);
+System.out.println(y);
+	}
+
+}
---
+++ JavaAutomation/src/JavaPrograms/firstdayProgram.java
@@ -0,0 +1,10 @@
+package JavaPrograms;
+
+public class firstdayProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Well come to Automation");
+	}
+
+}
